a19159993fda1d1475f0db3eae18399715585965
Andrew Mihal <acmihal@users.sourceforge.net>
2005-12-11 06:52:35 +0000

    Updated NEWS and the version number for release 2.5.

--- src/win32helpers/win32config.h
+++ src/win32helpers/win32config.h
@@ -8,13 +8,13 @@
 #define PACKAGE_NAME "enblend"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "enblend 2.4"
+#define PACKAGE_STRING "enblend 2.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "enblend"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.4"
+#define PACKAGE_VERSION "2.5"
 
 /* Version number of package */
-#define VERSION "2.4"
+#define VERSION "2.5"
