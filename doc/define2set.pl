82dd8a49b8b91a90f88eaf33565021ecc818f078
Kornel Benko <Kornel Benko@localhost>
2009-09-23 20:33:24 +0200

    Added creation of 	    varsenblend.texi, varsenfuse.texi config-h.texi 	    photographic-workflow.txt local-analysis-window.txt focus-stack-decision-tree.txt

---
+++ doc/define2set.pl
@@ -0,0 +1,11 @@
+#! /usr/bin/env perl
+
+use strict;
+
+while ( my $l = <> ) {
+    next if ($l !~ /^\s*#define/);
+    chomp($l);
+    $l =~ s/^\s*#define\s+(\S+)\s+"?([^"]*)"?/\@set CFG::$1 $2/;
+    print "$l\n";
+}
+exit(0);
