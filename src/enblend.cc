b48265b6d7e7673026a9dbc675d77f72d57c1cea
Dr. Christoph L. Spiel <cspiel@freenet.de>
2009-10-27 07:41:31 +0100

    Flush output channels when using both narrow and wide. Problem found by Thomas Modes.

--- src/enblend.cc
+++ src/enblend.cc
@@ -355,12 +355,14 @@ void printVersionAndExit(int argc, char** argv) {
     }
 
     if (Verbose >= VERBOSE_SIGNATURE_REPORTING) {
+        cout.flush();
         std::wcout << sig.message() << L"\n\n";
+        std::wcout.flush();
     }
 
     cout <<
         "Copyright (C) 2004-2009 Andrew Mihal.\n" <<
-        "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>\n" <<
+        "License GPLv2+: GNU GPL version 2 or later <http://www.gnu.org/licenses/gpl.html>\n" <<
         "This is free software: you are free to change and redistribute it.\n" <<
         "There is NO WARRANTY, to the extent permitted by law.\n" <<
         "\n" <<
