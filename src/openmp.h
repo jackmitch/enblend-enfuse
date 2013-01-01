c13059d9d5e67a7dc781b91de633458f41970d9e
Chris <cspiel@users.sourceforge.net>
2013-01-01 18:51:30 +0100

    Introduce the `restrict' keyword and use it in the Felzenschwalb-Huttenlocher algorithm.

--- src/openmp.h
+++ src/openmp.h
@@ -244,7 +244,7 @@ namespace detail
 
         int id() const {return 0;}
 
-        void operator()(ValueType*, const ValueType*, int) const
+        void operator()(ValueType* /* restrict d */, const ValueType* /* restrict f */, int /* n */) const
         {
             vigra_fail("fh::detail::ChessboardTransform1D: not implemented");
         }
@@ -258,7 +258,7 @@ namespace detail
 
         int id() const {return 1;}
 
-        void operator()(ValueType* d, const ValueType* f, int n) const
+        void operator()(ValueType* restrict d, const ValueType* restrict f, int n) const
         {
             const ValueType one = static_cast<ValueType>(1);
 
@@ -282,7 +282,7 @@ namespace detail
 
         int id() const {return 2;}
 
-        void operator()(ValueType* d, const ValueType* f, int n) const
+        void operator()(ValueType* restrict d, const ValueType* restrict f, int n) const
         {
             typedef float math_t;
 
