ed86accbd3a10f77aaf13fd6784d3264caa8c6f5
Chris <cspiel@users.sourceforge.net>
2014-08-27 10:10:33 +0200

    Laet Enblend and Enfuse print information on environment variables when called with `--help'.
    Reformat and augment the help screens for Enblend and Enfuse such that
    help2man(1) can produce handsome man pages.

--- src/enfuse.cc
+++ src/enfuse.cc
@@ -475,6 +475,7 @@ printUsage(const bool error = true)
         "INPUT... are image filenames or response filenames.  Response\n" <<
         "filenames start with an \"" << RESPONSE_FILE_PREFIX_CHAR << "\" character.\n"
         "\n" <<
+        "Options:\n" <<
         "Common options:\n" <<
         "  -l, --levels=LEVELS    limit number of blending LEVELS to use (1 to " << MAX_PYRAMID_LEVELS << ");\n" <<
         "                         negative number of LEVELS decreases maximum;\n" <<
@@ -630,6 +631,25 @@ printUsage(const bool error = true)
         "\n" <<
         "Enfuse accepts arguments to any option in uppercase as\n" <<
         "well as in lowercase letters.\n" <<
+        "\n" <<
+        "Environment:\n" <<
+#ifdef CACHE_IMAGES
+        "  TMPDIR                 The TMPDIR environment variable points to the directory,\n" <<
+        "                         where to store ImageCache files.  If unset Enfuse uses \"/tmp\".\n" <<
+#endif
+#ifdef OPENMP
+        "  OMP_NUM_THREADS        The OMP_NUM_THREADS environment variable sets the number\n" <<
+        "                         of threads to use in OpenMP parallel regions.  If unset\n" <<
+        "                         Enfuse uses as many threads as there are CPUs.\n" <<
+        "  OMP_DYNAMIC            The OMP_DYNAMIC environment variable controls dynamic\n" <<
+        "                         adjustment of the number of threads to use in executing\n" <<
+        "                         OpenMP parallel regions.\n" <<
+#endif
+#ifdef OPENCL
+        "  ENBLEND_OPENCL_PATH    The ENBLEND_OPENCL_PATH environment variable sets the search\n" <<
+        "                         path for OpenCL source files.  Note that the variable name is\n" <<
+        "                         ENBLEND_OPENCL_PATH for Enfuse, too." <<
+#endif
         "\n" <<
         "Report bugs at <" PACKAGE_BUGREPORT ">." <<
         std::endl;
