70649c3a084fa025c2d71cfb1481a12f2fd43640
Chris <cspiel@users.sourceforge.net>
2014-09-07 14:30:44 +0200

    Clean up Enblend's module "path.h".
    - Prefer long variables names to unnecessary abbreviations.
    - Use std::array<>, where beneficial.
    - Convert all unnecessary dynamic allocations of already implicitly
      dynamic containers into local allocations.
    - Straighten type promotions of the arguments to std::max<>.
    
    No functional change.

--- src/path.h
+++ src/path.h
@@ -25,6 +25,7 @@
 #include <config.h>
 #endif
 
+#include <array>
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -33,81 +34,85 @@
 namespace enblend
 {
     template <typename Point, typename Image>
-    class PathCompareFunctor
+    class PathCompareFunctor : public std::binary_function<Point, Point, bool>
     {
     public:
-        explicit PathCompareFunctor(const Image* i) : image(i) {}
+        explicit PathCompareFunctor(const Image* an_image) : image_(an_image) {}
 
-        bool operator()(const Point& a, const Point& b) const {
+        bool operator()(const Point& a_point, const Point& another_point) const {
             if (parameter::as_boolean("debug-path-compare", false)) {
                 std::cout << "+ PathCompareFunctor::operator(): comparing "
-                          << "a = (" << a.x << ", " << a.y << ") = " << (*image)[a]
-                          << ", b = (" << b.x << ", " << b.y << ") = " << (*image)[b] << std::endl;
+                          << "cost(p1 = " << a_point << ") = " << (*image_)[a_point] << " and "
+                          << "cost(p2 = " << another_point << ") = " << (*image_)[another_point]
+                          << std::endl;
             }
             // want the priority queue sorted in ascending order
-            return (*image)[a] > (*image)[b];
+            return (*image_)[a_point] > (*image_)[another_point];
         }
 
     private:
-        const Image* image;
+        const Image* const image_;
     }; // class PathCompareFunctor
 
 
     template <class CostImageIterator, class CostAccessor>
     std::vector<vigra::Point2D>*
-    minCostPath(CostImageIterator cost_upperleft, CostImageIterator cost_lowerright, CostAccessor ca,
-                vigra::Point2D startingPoint, vigra::Point2D endingPoint)
+    minCostPath(CostImageIterator cost_upperleft, CostImageIterator cost_lowerright, CostAccessor cost_accessor,
+                const vigra::Point2D& startingPoint, const vigra::Point2D& endingPoint)
     {
         typedef typename CostAccessor::value_type CostPixelType;
         typedef typename vigra::NumericTraits<CostPixelType>::Promote WorkingPixelType;
         typedef vigra::BasicImage<WorkingPixelType> WorkingImageType;
         typedef std::priority_queue<vigra::Point2D, std::vector<vigra::Point2D>,
-                                    PathCompareFunctor<vigra::Point2D, WorkingImageType> > PQ;
+                                    PathCompareFunctor<vigra::Point2D, WorkingImageType> > PriorityQueue;
 
-        const int w = cost_lowerright.x - cost_upperleft.x;
-        const int h = cost_lowerright.y - cost_upperleft.y;
+        const int width = cost_lowerright.x - cost_upperleft.x;
+        const int height = cost_lowerright.y - cost_upperleft.y;
 
         // 4-bit direction encoding {up, down, left, right}
         // A  8  9
         // 2  0  1
         // 6  4  5
-        //const unsigned char neighborArray[] = {0xA, 8, 9, 1, 5, 4, 6, 2};
-        const vigra::UInt8 neighborArray[] = {0xA, 1, 6, 8, 5, 2, 9, 4};
-        //const unsigned char neighborArrayInverse[] = {5, 4, 6, 2, 0xA, 8, 9, 1};
-        const vigra::UInt8 neighborArrayInverse[] = {5, 2, 9, 4, 0xA, 1, 6, 8};
+        //
+        // ANTICIPATED CHANGE: Check whether it is faster to pull out
+        // `neighborArray' and `neighborArrayInverse' as `static
+        // const'.
+        const std::array<vigra::UInt8, 8> neighborArray = {0xA, 1, 6, 8, 5, 2, 9, 4};
+        const std::array<vigra::UInt8, 8> neighborArrayInverse = {5, 2, 9, 4, 0xA, 1, 6, 8};
 
-        vigra::UInt8Image* pathNextHop = new vigra::UInt8Image(w, h, vigra::UInt8(0));
-        WorkingImageType* costSoFar = new WorkingImageType(w, h, vigra::NumericTraits<WorkingPixelType>::max());
-        PQ* pq = new PQ(PathCompareFunctor<vigra::Point2D, WorkingImageType>(costSoFar));
+        vigra::UInt8Image pathNextHop(width, height);
+        WorkingImageType costSoFar(width, height, vigra::NumericTraits<WorkingPixelType>::max());
+        // The doubled parentheses in the definition of `pq' avoid "C++'s most-vexing parse", gosh!
+        PriorityQueue pq((PathCompareFunctor<vigra::Point2D, WorkingImageType>(&costSoFar)));
         std::vector<vigra::Point2D>* result = new std::vector<vigra::Point2D>;
 
         if (parameter::as_boolean("debug-path", false)) {
-            std::cout << "+ minCostPath: w = " << w << ", h = " << h << "\n"
+            std::cout << "+ minCostPath: width = " << width << ", height = " << height << "\n"
                       << "+ minCostPath: startingPoint = " << startingPoint
                       << ", endingPoint = " << endingPoint << std::endl;
         }
 
-        (*costSoFar)[endingPoint] =
+        costSoFar[endingPoint] =
             std::max(vigra::NumericTraits<WorkingPixelType>::one(),
-                     vigra::NumericTraits<CostPixelType>::toPromote(ca(cost_upperleft + endingPoint)));
-        pq->push(endingPoint);
+                     vigra::NumericTraits<WorkingPixelType>::toPromote(cost_accessor(cost_upperleft + endingPoint)));
+        pq.push(endingPoint);
 
-        while (!pq->empty()) {
-            vigra::Point2D top = pq->top();
-            pq->pop();
+        while (!pq.empty()) {
+            vigra::Point2D top = pq.top();
+            pq.pop();
             if (parameter::as_boolean("debug-path", false)) {
                 std::cout << "+ minCostPath: visiting point = " << top << std::endl;
             }
 
             if (top != startingPoint) {
-                WorkingPixelType costToTop = (*costSoFar)[top];
+                WorkingPixelType costToTop = costSoFar[top];
                 if (parameter::as_boolean("debug-path", false)) {
                     std::cout << "+ minCostPath: costToTop = " << costToTop << std::endl;
                 }
 
                 // For each 8-neighbor of top with costSoFar == 0 do relax
                 for (int i = 0; i < 8; i++) {
-                    // get the negihbor;
+                    // Get the neighbor
                     vigra::UInt8 neighborDirection = neighborArray[i];
                     vigra::Point2D neighborPoint = top;
                     if (neighborDirection & 0x8) {--neighborPoint.y;}
@@ -116,8 +121,8 @@ namespace enblend
                     if (neighborDirection & 0x1) {++neighborPoint.x;}
 
                     // Make sure neighbor is in valid region
-                    if (neighborPoint.y < 0 || neighborPoint.y >= h
-                        || neighborPoint.x < 0 || neighborPoint.x >= w) {
+                    if (neighborPoint.y < 0 || neighborPoint.y >= height
+                        || neighborPoint.x < 0 || neighborPoint.x >= width) {
                         continue;
                     }
                     if (parameter::as_boolean("debug-path", false)) {
@@ -127,7 +132,7 @@ namespace enblend
                     // See if the neighbor has already been visited.
                     // If neighbor has maximal cost, it has not been visited.
                     // If so skip it.
-                    WorkingPixelType neighborPreviousCost = (*costSoFar)[neighborPoint];
+                    WorkingPixelType neighborPreviousCost = costSoFar[neighborPoint];
                     if (parameter::as_boolean("debug-path", false)) {
                         std::cout <<
                             "+ minCostPath: neighborPreviousCost = " << neighborPreviousCost << std::endl;
@@ -138,7 +143,7 @@ namespace enblend
 
                     WorkingPixelType neighborCost =
                         std::max(vigra::NumericTraits<WorkingPixelType>::one(),
-                                 vigra::NumericTraits<CostPixelType>::toPromote(ca(cost_upperleft + neighborPoint)));
+                                 vigra::NumericTraits<WorkingPixelType>::toPromote(cost_accessor(cost_upperleft + neighborPoint)));
                     if (parameter::as_boolean("debug-path", false)) {
                         std::cout << "+ minCostPath: neighborCost = " << neighborCost << std::endl;
                     }
@@ -154,21 +159,21 @@ namespace enblend
                     const WorkingPixelType newNeighborCost = neighborCost + costToTop;
                     if (newNeighborCost < neighborPreviousCost) {
                         // We have found the shortest path to neighbor.
-                        (*costSoFar)[neighborPoint] = newNeighborCost;
-                        (*pathNextHop)[neighborPoint] = neighborArrayInverse[i];
-                        pq->push(neighborPoint);
+                        costSoFar[neighborPoint] = newNeighborCost;
+                        pathNextHop[neighborPoint] = neighborArrayInverse[i];
+                        pq.push(neighborPoint);
                     }
                 }
             } else {
                 // If yes then follow back to beginning using pathNextHop
                 // include neither start nor end point in result
-                vigra::UInt8 nextHop = (*pathNextHop)[top];
+                vigra::UInt8 nextHop = pathNextHop[top];
                 while (nextHop != 0) {
                     if (nextHop & 0x8) {--top.y;}
                     if (nextHop & 0x4) {++top.y;}
                     if (nextHop & 0x2) {--top.x;}
                     if (nextHop & 0x1) {++top.x;}
-                    nextHop = (*pathNextHop)[top];
+                    nextHop = pathNextHop[top];
                     if (nextHop != 0) {
                         result->push_back(top);
                     }
@@ -177,10 +182,6 @@ namespace enblend
             }
         }
 
-        delete pathNextHop;
-        delete costSoFar;
-        delete pq;
-
         return result;
     }
 
@@ -188,7 +189,7 @@ namespace enblend
     template <class CostImageIterator, class CostAccessor>
     inline static std::vector<vigra::Point2D>*
     minCostPath(vigra::triple<CostImageIterator, CostImageIterator, CostAccessor> cost,
-                vigra::Point2D startingPoint, vigra::Point2D endingPoint)
+                const vigra::Point2D& startingPoint, const vigra::Point2D& endingPoint)
     {
         return minCostPath(cost.first, cost.second, cost.third, startingPoint, endingPoint);
     }
