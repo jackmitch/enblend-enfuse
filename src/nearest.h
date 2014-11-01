588c7e06e11f9d4657c302921b2aea26d7547fb7
Chris <cspiel@users.sourceforge.net>
2014-11-01 15:57:38 +0100

    Parameterize the overlap threshold with         overlap-check-threshold Prefer `vigra::Siz2D' to `vigra::Diff2D'.

--- src/nearest.h
+++ src/nearest.h
@@ -429,8 +429,7 @@ nearestFeatureTransform(SrcImageIterator src1_upperleft, SrcImageIterator src1_l
     typedef vigra::NumericTraits<DestPixelType> DestPixelTraits;
 
     const SrcPixelType background = SrcPixelTraits::zero();
-    const vigra::Diff2D size(src1_lowerright.x - src1_upperleft.x,
-                             src1_lowerright.y - src1_upperleft.y);
+    const vigra::Size2D size(src1_lowerright.x - src1_upperleft.x, src1_lowerright.y - src1_upperleft.y);
 
     IMAGETYPE<float> dist12(size);
     IMAGETYPE<float> dist21(size);
@@ -451,20 +450,17 @@ nearestFeatureTransform(SrcImageIterator src1_upperleft, SrcImageIterator src1_l
         // consider the overlap of the masks to be complete, i.e. the
         // image pair as useless.
         //
-        // The current value is 2x the circumference of the overlap
-        // rectangle.
-        2U * 2U * (static_cast<unsigned>(size.x) + static_cast<unsigned>(size.y));
-    unsigned tally12;
-    unsigned tally21;
-
+        // The current parameter default is two times the
+        // circumference of the overlap rectangle.
+        parameter::as_unsigned("overlap-check-threshold", 2U) *
+        2U * (static_cast<unsigned>(size.height()) + static_cast<unsigned>(size.width()));
     IMAGETYPE<SrcPixelType> diff12(size);
     vigra::omp::combineTwoImages(src1_upperleft, src1_lowerright, sa1,
                                  src2_upperleft, sa2,
                                  diff12.upperLeft(), diff12.accessor(),
                                  saturating_subtract<SrcPixelType>());
 
-    tally12 = quick_tally(diff12.begin(), diff12.end(), diff12.accessor(),
-                          overlap_threshold);
+    const unsigned tally12 = quick_tally(diff12.begin(), diff12.end(), diff12.accessor(), overlap_threshold);
 
     switch (boundary)
     {
@@ -494,8 +490,7 @@ nearestFeatureTransform(SrcImageIterator src1_upperleft, SrcImageIterator src1_l
                                  diff21.upperLeft(), diff21.accessor(),
                                  saturating_subtract<SrcPixelType>());
 
-    tally21 = quick_tally(diff21.begin(), diff21.end(), diff21.accessor(),
-                          overlap_threshold);
+    const unsigned tally21 = quick_tally(diff21.begin(), diff21.end(), diff21.accessor(), overlap_threshold);
 
     switch (boundary)
     {
@@ -524,14 +519,13 @@ nearestFeatureTransform(SrcImageIterator src1_upperleft, SrcImageIterator src1_l
     if (overlap_tally < overlap_threshold)
     {
         std::cerr << "\n" <<
-            command << ": excessive overlap detected; remove one of the images\n";
-#ifdef DEBUG_NEAREST_FEATURE_TRANSFORM
-        std::cout <<
-            "+ nearestFeatureTransform: overlap area size = " << size << "\n" <<
-            "+ nearestFeatureTransform: threshold is " << overlap_threshold <<
-            " pixels for this pair of images\n" <<
-            "+ nearestFeatureTransform: only " << overlap_tally << " of " <<
-            size.x * size.y << " pixels do not overlap\n";
+            command << ": excessive image overlap detected; too high risk of defective seam line\n" <<
+            command << ": info:     remove one of the images" << std::endl;
+#ifdef DEBUG
+        std::cerr <<
+            command << ": info: overlap area size is " << size << " > " << overlap_threshold << ", but only\n" <<
+            command << ": info: " << overlap_tally << " of " << size.x * size.y << " pixels do not overlap" <<
+            std::endl;
 #endif
         exit(1);
     }
